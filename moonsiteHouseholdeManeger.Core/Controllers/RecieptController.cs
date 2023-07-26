89e38db6d1a0a5172ceb4678391dc585daef137d
romanbishof <bishofkristina@gmail.com>
2023-07-26 21:51:20 +0300

    from c# to umbraco

---
+++ moonsiteHouseholdeManeger.Core/Controllers/RecieptController.cs
@@ -0,0 +1,28 @@
+﻿using moonsiteHouseholdeManeger.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace moonsiteHouseholdeManeger.Core.Controllers
+{
+    //all operations regarding reciept form
+    public class RecieptController: SurfaceController
+    {
+        public ActionResult RenderRecieptForm()
+        {
+            var vm = new RecieptFormViewModel();
+
+            return PartialView("~/Views/Partials/RecieptForm.cshtml", vm);
+        }
+
+        [HttpPost]
+        public ActionResult HandleRecieptForm(RecieptFormViewModel vm)
+        {
+            return null;
+        }
+    }
+}
---
+++ moonsiteHouseholdeManeger.Core/ViewModels/RecieptFormViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moonsiteHouseholdeManeger.Core.ViewModels
+{
+    public class RecieptFormViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Tenant name is required.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Tenant name should contain only letters and spaces.")]
+        public string NameOfTenent { get; set; }
+
+        [Required(ErrorMessage = "Please select an apartment")]
+        public int Apartment { get; set; }
+        public DateTime DateOfPayment { get; set; }
+
+        [Required(ErrorMessage = "Please select payment method")]
+        public string PaymentMethod { get; set; }
+
+        [Required(ErrorMessage = "Please write the amount of payment")]
+        [RegularExpression(@"^[1-9][0-9]*$", ErrorMessage = "Payment amount should contain only numbers and cannot be 0.")]
+        public int PaymentAmount { get; set; }
+        public string Month { get; set; }
+
+
+        [Required(ErrorMessage = "Please choose month/s of payment")]
+        public List<string> Monthes { get; set; }
+    }
+}
